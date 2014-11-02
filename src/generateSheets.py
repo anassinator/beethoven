39ebf95155f73def0211f51a53f1c69f5fd41c1a
Miles Cranmer <miles.cranmer@gmail.com>
2014-11-02 01:02:41 -0400

    ikw

--- src/generateSheets.py
+++ src/generateSheets.py
@@ -2,20 +2,20 @@ import midi2lily
 import subprocess
 import shlex
 
-parser = midi2lily.MidiParser("test2.midi")
-f = open("temp.ly",'w')
-f.write("\\version \"2.16.2\"\n")
-for x in range(len(parser.pattern)):
-    track= parser.getFilteredTrack(x)
-    note_list = parser.parseTrack(track)
-    #print note_list
-    if note_list:
-        output = midi2lily.LyGenerator(note_list).output
-        f.write("\\new Staff {\n")
-        f.write(output)
-        f.write("\n}\n\n")
-cmd = "lilypond temp.ly"
-subprocess.Popen(shlex.split(cmd))
-cmd = "rm temp.ly"
-subprocess.Popen(shlex.split(cmd))
+def makePDF (filename,out):
+	parser = midi2lily.MidiParser(filename+".midi")
+	f = open(out+".ly",'w')
+	f.write("\\version \"2.16.2\"\n")
+	for x in range(len(parser.pattern)):
+	    track= parser.getFilteredTrack(x)
+	    note_list = parser.parseTrack(track)
+	    #print note_list
+	    if note_list:
+		output = midi2lily.LyGenerator(note_list).output
+		f.write("\\new Staff {\n")
+		f.write(output)
+		f.write("\n}\n\n")
+	cmd = "lilypond " + out + ".ly"
+	print cmd
+	subprocess.Popen(shlex.split(cmd))
 
